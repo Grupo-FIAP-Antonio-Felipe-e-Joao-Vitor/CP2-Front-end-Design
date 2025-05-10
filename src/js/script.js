5afba842c7b85840f13c64b5641d347b17fc2944
Antonio <antoniojacintoa@gmail.com>
2025-05-10 15:18:00 -0300

    criando e estilizando banner

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+const imgBanner = [
+    "./src/assets/imagem1.jpg",
+    "./src/assets/imagem2.jpg",
+    "./src/assets/imagem3.jpg",
+    "./src/assets/imagem4.jpg",
+]
+
+const campoBanner = document.getElementById("banner")
+let i = 0
+
+function slideShow() {
+    campoBanner.src = imgBanner[i]
+    i++
+    if (i == imgBanner.length) {
+        i = 0;
+    }
+    setTimeout(slideShow, 2000);
+}
+
+slideShow()
